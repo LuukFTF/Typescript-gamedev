5acdc970babe8b7972771881dba89099f5b0755e
Lucas van der Vegt <lpsva@DESKTOP-DMRTBKU.prometheusgroup.com>
2020-05-19 13:02:34 +0200

    init

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,16 +1,21 @@
 "use strict";
 var Car = (function () {
     function Car() {
-        console.log("vroom!");
+        console.log("car created");
     }
+    Car.prototype.update = function () {
+        console.log("vrooom!");
+    };
     return Car;
 }());
 var Game = (function () {
     function Game() {
         console.log("new game created!");
         var c = new Car();
+        c.update();
     }
     return Game;
 }());
 window.addEventListener("load", function () { return new Game(); });
+console.log("hello ts");
 //# sourceMappingURL=main.js.map
